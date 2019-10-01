a9337ec27a07a92ba29e1650dec724d828af3cea
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-01 01:23:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,6 @@
 first_number = 15
 second_number = 20
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient= first_number/second_number
